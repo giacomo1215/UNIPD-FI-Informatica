671e5b494098a524508418276e0688b14b3844cb
Giacomo Giorgi <giacomo1215@gmail.com>
2023-11-15 23:55:00 +0100

    Lesson 15/11/23

---
+++ java-files/SelectValue.java
@@ -0,0 +1,30 @@
+// problema esercizio in classe 15 novembre 2023
+import java.util.Scanner;
+
+public class SelectValue
+{
+    public static Scanner input = new Scanner(System.in);
+    
+    public static void main(String[] args)
+    {
+        double[] values = new double[10];
+        System.out.println("Inserisci dieci valori");
+        for (int i  = 0; i < values.length; i++)
+        {
+            values[i] = input.nextDouble();
+        }
+        
+        System.out.println("Inserisci un numreo:");
+        int index = input.nextInt();
+
+        if (index < 0 || index >= values.length)
+        {
+            System.out.println("Valore errato");
+        }
+        else
+        {
+            System.out.println(values[index]);
+        }
+        input.close();
+    }
+}
